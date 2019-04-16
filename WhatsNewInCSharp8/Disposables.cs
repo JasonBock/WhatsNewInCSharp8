93e9e8e04f10c4fbe618c7407ae64a4ffc815128
Jason Bock <jason@jasonbock.net>
2019-04-15 20:09:56 -0500

    Adding more examples

---
+++ WhatsNewInCSharp8/Disposables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WhatsNewInCSharp8
+{
+	public class DisposableService
+		: IDisposable
+	{
+		public void Dispose() => Console.WriteLine($"{nameof(DisposableService)} - I am disposed.");
+	}
+
+	public ref struct DisposableRefStruct
+	{
+		public void Dispose() => Console.WriteLine($"{nameof(DisposableRefStruct)} - I am disposed.");
+	}
+}
--- WhatsNewInCSharp8/Program.cs
+++ WhatsNewInCSharp8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace WhatsNewInCSharp8
 {
@@ -6,17 +7,58 @@ namespace WhatsNewInCSharp8
    {
 		static void Main() =>
 			//Program.DemonstrateNullableReferenceTypes();
-			Program.DemonstrateVerbatimInterpolatedStrings();
+			//Program.DemonstrateEnhancedUsing();
+			Program.DemonstrateRanges();
+			//Program.DemonstrateVerbatimInterpolatedStrings();
+
+		/*
+		static async Task Main() =>
+			await Program.DemonstrateAsyncStreams();
+		*/
 
 		private static void DemonstrateNullableReferenceTypes() { }
 
 		private static void DemonstrateRecursivePatterns() { }
 
-		private static void DemonstrateAsyncStreams() { }
+		// https://github.com/dotnet/csharplang/blob/master/proposals/csharp-8.0/async-streams.md
+		private static async Task DemonstrateAsyncStreams()
+		{
+			await foreach(var value in new AsynchronousRandom(10))
+			{
+				await Console.Out.WriteLineAsync(value.ToString());
+			}
+		}
 
-		private static void DemonstrateEnhancedUsing() { }
+		// https://github.com/dotnet/csharplang/blob/master/proposals/csharp-8.0/using.md
+		private static void DemonstrateEnhancedUsing()
+		{
+			using var _ = new DisposableService();
+			Console.WriteLine($"I am within the disposable scope of {nameof(DisposableService)}");
+			Console.WriteLine($"I am still within the disposable scope of {nameof(DisposableService)}");
 
-		private static void DemonstrateRanges() { }
+			using (var refStruct = new DisposableRefStruct())
+			{
+				Console.WriteLine($"I am within the disposable scope of {nameof(DisposableRefStruct)}");
+				Console.WriteLine($"I am still within the disposable scope of {nameof(DisposableRefStruct)}");
+			}
+		}
+
+		// https://github.com/dotnet/csharplang/blob/master/proposals/csharp-8.0/ranges.md
+		private static void DemonstrateRanges()
+		{
+			var values = new int[] { 3, 1, 4, 1, 5, 9, 2, 6, 5 };
+
+			Console.Out.WriteLine($"Everything: {string.Join(", ", values[..])}");
+			Console.Out.WriteLine($"From the 2nd to the 5th (exclusive): {string.Join(", ", values[2..5])}");
+			Console.Out.WriteLine($"Everything after the 3rd: {string.Join(", ", values[3..])}");
+			Console.Out.WriteLine($"Everything except the last: {string.Join(", ", values[..^1])}");
+			Console.Out.WriteLine($"Just the last: {string.Join(", ", values[^1])}");
+			Console.Out.WriteLine($"Just the second to the last: {string.Join(", ", values[^2])}");
+			Console.Out.WriteLine($"Just the third to the last: {string.Join(", ", values[^3])}");
+
+			var range = new Range(2, 5);
+			Console.Out.WriteLine($"From the 2nd to the 5th (exclusive) with a range: {string.Join(", ", values[range])}");
+		}
 
 		private static void DemonstrateDefaultInDeconstruction() { }
 
