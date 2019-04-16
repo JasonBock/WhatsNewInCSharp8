562d7713a1971c2790bdb9564262ba00b23da5d2
Jason Bock <jason@jasonbock.net>
2019-04-15 19:34:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# "What's New in C#8" Demo Code
+This repo contains the demo code used for my "What's New in C#8" talk.
---
+++ WhatsNewInCSharp8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace WhatsNewInCSharp8
+{
+   class Program
+   {
+		static void Main() =>
+			//Program.DemonstrateNullableReferenceTypes();
+			Program.DemonstrateVerbatimInterpolatedStrings();
+
+		private static void DemonstrateNullableReferenceTypes() { }
+
+		private static void DemonstrateRecursivePatterns() { }
+
+		private static void DemonstrateAsyncStreams() { }
+
+		private static void DemonstrateEnhancedUsing() { }
+
+		private static void DemonstrateRanges() { }
+
+		private static void DemonstrateDefaultInDeconstruction() { }
+
+		private static void DemonstrateNullCoalescingAssigments() { }
+
+		// https://github.com/dotnet/csharplang/issues/1630
+		private static void DemonstrateVerbatimInterpolatedStrings()
+		{
+			var value = new Random().Next();
+			var currentWay = $@"Here is the current way: {value}";
+			var newWay = @$"Here is the new way: {value}";
+
+			Console.WriteLine(currentWay);
+			Console.WriteLine(newWay);
+		}
+
+		private static void DemonstrateStaticLocalFunctions() { }
+	}
+}
